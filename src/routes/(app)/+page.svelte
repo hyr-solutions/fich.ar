95326874c0dda32362b02992fc09a6f50d51be74
hyr-solutions <rolon@hyr.solutions>
2024-11-15 13:58:26 -0300

    made landing responsive!

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,18 +5,18 @@
 
 <section class="min-h-screen bg-slate-800 bg-gradient-to-br from-slate-600 to-slate-950">
 	<div class="mx-auto w-full max-w-6xl p-4">
-		<h1 class="mb-16 mt-48 text-center text-6xl font-black text-slate-200">
-			Just build modern forms<br /> without ever leaving your HTML.
+		<h1 class="my-20 mt-36 sm:mt-48 text-center text-3xl sm:text-6xl font-black text-slate-200">
+			Just build modern forms<br class="hidden sm:inline" /> without ever leaving your HTML.
 		</h1>
 
-		<div class="flex gap-16">
-			<div class="flex w-[40rem] flex-col rounded-lg bg-slate-900 text-white">
+		<div class="flex flex-col sm:flex-row gap-6 sm:gap-16">
+			<div class="flex sm:w-[40rem] flex-col rounded-lg bg-slate-900 text-white">
 				<div class="flex gap-2 border-b-2 border-b-slate-700 px-3 py-3">
 					<div class="size-3 cursor-pointer rounded-full bg-slate-400 hover:bg-slate-500"></div>
 					<div class="size-3 cursor-pointer rounded-full bg-slate-400 hover:bg-slate-500"></div>
 					<div class="size-3 cursor-pointer rounded-full bg-slate-400 hover:bg-slate-500"></div>
 				</div>
-				<code class="overflow-hidden whitespace-pre p-4">
+				<code class="overflow-hidden whitespace-pre p-4 text-sm sm:text-base">
 					{`<iframe class="w-[40rem]" allowtransparency title="example-form" 
     src="/example-form
         ?form=
@@ -32,7 +32,7 @@
 				</code>
 			</div>
             <iframe
-				class="w-[40rem]"
+				class="w-full h-72"
 				allowtransparency
 				title="example-form"
 				src="/example-form
